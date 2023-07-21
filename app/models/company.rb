70f7ae1feb506cac380d707096c90400bb513351
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2023-07-21 21:32:27 +0900

    feat: 管理機能を追加 Close #4

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,4 +5,9 @@ VALID_PHONE_NUMBER_REGEX = /\A0\d{9,10}\z/
 validates :name, presence: true
 validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 validates :phone_number, presence: true, uniqueness: true, format: { with: VALID_PHONE_NUMBER_REGEX }
+
+def self.ransackable_attributes(auth_object = nil)
+  ["id", "name", "email", "phone_number", "created_at", "updated_at"]
+end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_10_203656) do
+ActiveRecord::Schema.define(version: 2023_07_20_142913) do
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.integer "resource_id"
+    t.string "author_type"
+    t.integer "author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name", null: false
