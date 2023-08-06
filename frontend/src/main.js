dde62b86c1b1b8cca0c3f89848838b1a81ff4df5
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2023-08-06 21:03:23 +0900

    feat: cssリセットのressをインストール #62

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,6 +12,9 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+// ress
+import 'ress'
+
 const vuetify = createVuetify({
   components,
   directives,
