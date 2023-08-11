2165163b7a4d2fbdda536e34cfe29b10be475144
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2023-08-11 14:50:16 +0900

    feat: バリデーションを作成 #60

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,8 @@
 class Client < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A(?!.*(--|\.-|-\.│\s@))[a-z+-.]+@(?!.*(--|\.-|-\.│\s@))[a-z.]+\.(?!.*(--|\.-|-\.│\s@))[a-z]+\z/
+  VALID_PHONE_NUMBER_REGEX = /\A0\d{9,10}\z/
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :phone_number, presence: true, uniqueness: true, format: { with: VALID_PHONE_NUMBER_REGEX }
 end
