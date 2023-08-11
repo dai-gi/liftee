18ae91ac1a614d9dda3dd5dde4efbc5700c28423
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2023-08-11 22:39:47 +0900

    feat: ClientモデルのAPIを作成 #23

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api, {format: 'json'} do
     namespace :v1 do
       resources :company, only: [:index, :show]
+      resources :client, only: [:index, :show]
     end
   end
 end
