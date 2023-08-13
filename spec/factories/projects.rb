50b7d47e87c221fc46733eb9db8ef5f583c073e9
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2023-08-13 10:24:21 +0900

    feat: Projectモデルを作成 #36

---
+++ spec/factories/projects.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :project do
+    name { "MyString" }
+    start_date { "2023-08-12" }
+    end_date { "2023-08-12" }
+    status { "" }
+    overview { "MyString" }
+  end
+end
