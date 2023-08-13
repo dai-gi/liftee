31af32f35aca70bcd2aa8b894c19c793fb7838cb
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2023-08-14 00:06:07 +0900

    feat: ActiveAdminでCRUDできるように設定 #73

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ApplicationRecord
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "end_date", "id", "name", "overview", "start_date", "status", "updated_at"]
+  end
 end
