5ff60234a32a17fe71e4fc5000ca3340e9f4249b
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2023-08-15 08:16:51 +0900

    feat: project情報をVueで表示 #73

--- frontend/src/components/ProjectList.vue
+++ frontend/src/components/ProjectList.vue
@@ -2,12 +2,20 @@
   import axios from 'axios';
   import { onMounted, ref } from 'vue';
 
-  const clientList = ref([]);
+  const clients = ref([]);
+  const projects = ref([]);
 
   onMounted(async () => {
     try{
       const res = await axios.get('http://localhost:3000/api/v1/client');
-      clientList.value = res.data;
+      clients.value = res.data;
+    } catch(error) {
+      console.log('Error: ', error);
+    }
+
+    try{
+      const res = await axios.get('http://localhost:3000/api/v1/project');
+      projects.value = res.data;
     } catch(error) {
       console.log('Error: ', error);
     }
@@ -16,15 +24,41 @@
 
 <template>
   <v-row>
-    <v-col v-for="client in clientList" :key="client" cols="12">
-      <h4 class="d-flex flex-row">{{ client.name }}</h4>
-      <v-card class="d-flex align-center">
-        <p class="pa-4">プロジェクト名</p>
-        <p class="pa-4">概要</p>
-        <p class="pa-4">期間</p>
-        <p class="pa-4">ステータス</p>
-        <v-icon class="px-5" icon="mdi-dots-vertical"></v-icon>
-      </v-card>
+    <v-col v-for="client in clients" :key="client" cols="12" class="mb-5">
+      <h3 class="d-flex flex-row ma-2">{{ client.name }}</h3>
+      <template v-for="project in projects" :key="project">
+        <router-link to="/project">
+          <template v-if="client.id === project.client_id">
+            <v-card v-click-outside="onClickOutside" @click="active = true" :color="active ? 'grey-lighten-4' : undefined" :dark="active" class="d-flex align-center ma-3 pa-3">
+              <v-row>
+                <v-col cols="3">
+                  <v-card-subtitle class="pb-3">プロジェクト名</v-card-subtitle>
+                  <v-card-text class="pa-0 text-truncate">{{ project.name }}</v-card-text>
+                </v-col>
+                <v-col cols="3">
+                  <v-card-subtitle class="pb-3">概要</v-card-subtitle>
+                  <v-card-text class="pa-0 text-truncate">{{ project.overview }}</v-card-text>
+                </v-col>
+                <v-col cols="3">
+                  <v-card-subtitle class="pb-3">期間</v-card-subtitle>
+                  <v-card-text class="pa-0">{{ project.start_date }} 〜 {{ project.end_date }}</v-card-text>
+                </v-col>
+                <v-col cols="2">
+                  <v-card-subtitle class="pb-3">ステータス</v-card-subtitle>
+                  <v-card-text class="pa-0">{{ project.status }}</v-card-text>
+                </v-col>
+                <v-col cols="1" class="d-flex align-center justify-center">
+                  <v-btn size="small" variant="text" icon="mdi-dots-vertical">
+                    <template v-slot>
+                      <v-icon color="grey-lighten-1"></v-icon>
+                    </template>
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-card>
+          </template>
+        </router-link>
+      </template>
     </v-col>
   </v-row>
 </template>
