384954f37cd720408b5a39c1c7d6b1710e2c3d4c
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2023-08-14 15:54:30 +0900

    feat: バリデーションを設定 #73

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,10 @@
 class Project < ApplicationRecord
 
+  validates :name, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :status, presence: true
+
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "end_date", "id", "name", "overview", "start_date", "status", "updated_at"]
   end
