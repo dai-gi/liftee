672b49205501e0ce4b8eb7ef42fbeafa838440fd
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2023-08-14 16:25:43 +0900

    feat: ProjectモデルのAPIを作成 #73

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :company, only: [:index, :show]
       resources :client, only: [:index, :show]
+      resources :project, only: [:index, :show]
     end
   end
 end
