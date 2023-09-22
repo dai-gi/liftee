76b660d83fabc6a02ab836e3c18de051b39fa74d
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2023-09-22 20:52:21 +0900

    feat: userモデルにバリデーションを設定 #46

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  belongs_to :company
+
+  enum role: { general: 0, project_manager: 1, admin: 2 }
+
+  validates :name, presence: true
+  validates :syllabic_characters, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone_number, presence: true
+  validates :password_digest, presence: true
+  validates :role, presence: true
 end
