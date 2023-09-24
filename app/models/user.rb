36a146e908a98b6efab2b365cc2f73abac43c4d0
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2023-09-24 10:53:57 +0900

    feat: ユーザ作成でパスワードがハッシュ化されるようにライブラリを追加 #46

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+
   belongs_to :company
 
   enum role: { general: 0, project_manager: 1, admin: 2 }
 
   validates :name, presence: true
   validates :syllabic_characters, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
   validates :phone_number, presence: true
-  validates :password_digest, presence: true
   validates :role, presence: true
 
   def self.ransackable_attributes(auth_object = nil)
