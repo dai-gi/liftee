a3d4de823b717f3b2a0b6d788b4d5d7556db87e8
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2023-09-24 10:56:31 +0900

    feat: 管理画面のユーザー入力フォームをカスタマイズ #46

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register User do
-  permit_params :name, :syllabic_characters, :email, :phone_number, :password_digest, :role, :company_id
+  permit_params :name, :syllabic_characters, :email, :phone_number, :password, :password_confirmation, :role, :company_id
+
+  form do |f|
+    f.inputs "User Details" do
+      f.input :company_id, as: :select, collection: Company.all.map { |company| [company.name, company.id] }, label: 'Company Name'
+      f.input :name
+      f.input :syllabic_characters
+      f.input :email
+      f.input :phone_number
+      f.input :password
+      f.input :password_confirmation
+      f.input :role
+    end
+    f.actions
+  end
 end
